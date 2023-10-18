97f5088741b0e7721b0476220d7142b4e67b7666
Blag <atejada@gmail.com>
2023-10-18 01:13:22 -0400

    Update ReadInbox.php

--- ReadInbox.php
+++ ReadInbox.php
@@ -10,16 +10,21 @@ $dotenv->load();
 $headr = array();
 $headr[] = 'Accept: application/json';
 $headr[] = 'Content-type: application/json';
-$headr[] = 'Authorization: Bearer ' . $_ENV['ACCESS_TOKEN'];
+$headr[] = 'Authorization: Bearer ' . $_ENV['API_KEY_V3'];
 
+$url = "https://api.us.nylas.com/v3/grants/" . $_ENV['GRANT_ID'] . "/messages?limit=1";
 # Call the Read Email API
-$ch = curl_init( "https://api.nylas.com/messages?in=inbox&unread=true&limit=1" );
+$ch = curl_init( $url );
+#echo var_dump($ch);
+#die();
 # Submit the Headers
 curl_setopt( $ch, CURLOPT_HTTPHEADER, $headr);
 # Return response instead of printing
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 # Send request
 $result = curl_exec($ch);
+echo var_dump($result);
+#die();
 # Close request
 curl_close($ch);
 # Print response.
